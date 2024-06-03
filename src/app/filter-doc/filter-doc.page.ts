8c2a31248878cee559e94be4dbd89b76d613c5b6
AbdoHerO <abdo.sbeta.12@gmail.com>
2024-06-03 16:28:36 +0100

    filter after before

--- src/app/filter-doc/filter-doc.page.ts
+++ src/app/filter-doc/filter-doc.page.ts
@@ -6,22 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-doc.page.scss'],
 })
 export class FilterDocPage implements OnInit {
-
   isScanning: boolean = false;
   hideAnimation: boolean = false;
+
+  isScanning_2: boolean = false;
+  hideAnimation_2: boolean = false;
 
   constructor() {}
 
   applyFilter() {
     this.isScanning = !this.isScanning;
     if (this.isScanning) {
-    setTimeout(() => {
-      this.hideAnimation = true
-    }, 2000);
-    this.hideAnimation = false
+      setTimeout(() => {
+        this.hideAnimation = true;
+      }, 2000);
+      this.hideAnimation = false;
+    }
   }
-}
-  
-  ngOnInit() {
+
+  applyFilter_2() {
+    this.isScanning_2 = !this.isScanning_2;
+    if (this.isScanning_2) {
+      setTimeout(() => {
+        this.hideAnimation_2 = true;
+      }, 2000);
+      this.hideAnimation_2 = false;
+    }
   }
+
+  ngOnInit() {}
 }
